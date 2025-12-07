03f08dd8d2eca3817a24ed0b0ca290005ef5b700
zoomer <zoomer@laptop.com>
2025-12-07 19:47:53 +0100

    change obsidian theme to nord theme

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -1,9 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.obsidian = {
     enable = true;
     vaults = {
-      "thoughts haven".enable = true;
+      "thoughts haven" = {
+        enable = true;
+        settings = {
+          appearance = {
+            cssTheme = "Nord";
+          };
+          themes = [
+            {
+              pkg = pkgs.fetchFromGitHub {
+                owner = "insanum";
+                repo = "obsidian_nord";
+                rev = "master";
+                hash = "sha256-LdjxEpxnsV4YXI9K72o2m6E1nhZvKb/r/WCuVKihugI=";
+                name = "Nord";
+              };
+              enable = true;
+            }
+          ];
+
+        };
+      };
     };
     defaultSettings.themes = [ "gruvbox" ];
 
